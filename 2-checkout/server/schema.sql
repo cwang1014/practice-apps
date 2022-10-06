97b22774f8cd0990adba5907c267cfebb6182eab
Curtis Wang <curtis.wang10@gmail.com>
2022-10-06 10:32:27 -0700

    npm installed and set up basic schema.sql

---
+++ 2-checkout/server/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS checkout;
+CREATE DATABASE checkout;
+USE checkout;
+
+CREATE TABLE form1 {
+
+};
+
+CREATE TABLE form2 {
+
+};
+
+CREATE TABLE form3 {
+
+};
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
