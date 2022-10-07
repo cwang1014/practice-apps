8708f51aacc417977283cb739485c5250bef73f7
Curtis Wang <curtis.wang10@gmail.com>
2022-10-07 10:22:27 -0700

    changing from 3 tables to 1

--- 2-checkout/server/schema.sql
+++ 2-checkout/server/schema.sql
@@ -2,33 +2,43 @@ DROP DATABASE IF EXISTS checkout;
 CREATE DATABASE checkout;
 USE checkout;
 
-CREATE TABLE IF NOT EXISTS form1 (
+CREATE TABLE IF NOT EXISTS form (
   sessionid VARCHAR(150) NOT NULL,
   name VARCHAR(40) NOT NULL,
   email VARCHAR(60) NOT NULL UNIQUE,
   password VARCHAR(16) NOT NULL,
-  PRIMARY KEY (sessionid)
-);
-
-CREATE TABLE IF NOT EXISTS form2 (
-  sessionid VARCHAR(150) NOT NULL,
   addressline1 VARCHAR(60) NOT NULL,
   addressline2 VARCHAR(60),
   city VARCHAR(60) NOT NULL,
   state VARCHAR(30) NOT NULL,
   zip VARCHAR(5) NOT NULL,
   phone VARCHAR(10) NOT NULL,
-  PRIMARY KEY (sessionid)
-);
-
-CREATE TABLE IF NOT EXISTS form3 (
-  sessionid VARCHAR(150) NOT NULL,
   ccnumber VARCHAR(40) NOT NULL,
   expiration VARCHAR(10) NOT NULL,
   cvv VARCHAR(10) NOT NULL,
   billingzip VARCHAR(5) NOT NULL,
   PRIMARY KEY (sessionid)
 );
+
+-- CREATE TABLE IF NOT EXISTS form2 (
+--   sessionid VARCHAR(150) NOT NULL,
+--   addressline1 VARCHAR(60) NOT NULL,
+--   addressline2 VARCHAR(60),
+--   city VARCHAR(60) NOT NULL,
+--   state VARCHAR(30) NOT NULL,
+--   zip VARCHAR(5) NOT NULL,
+--   phone VARCHAR(10) NOT NULL,
+--   PRIMARY KEY (sessionid)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS form3 (
+--   sessionid VARCHAR(150) NOT NULL,
+--   ccnumber VARCHAR(40) NOT NULL,
+--   expiration VARCHAR(10) NOT NULL,
+--   cvv VARCHAR(10) NOT NULL,
+--   billingzip VARCHAR(5) NOT NULL,
+--   PRIMARY KEY (sessionid)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
