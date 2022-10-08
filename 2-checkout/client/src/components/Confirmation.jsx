c8211bcd495b7e5da6a65cf461f6e74371309fba
Curtis Wang <curtis.wang10@gmail.com>
2022-10-07 21:21:10 -0700

    really basic implementation of the checkout app, will try some refining tomorrow then redo a sprint like glossary with both databases

--- 2-checkout/client/src/components/Confirmation.jsx
+++ 2-checkout/client/src/components/Confirmation.jsx
@@ -3,12 +3,11 @@ import axios from 'axios';
 
 const ConfirmationForm = ({ formNum, setFormNum }) => {
 
-  // const [formData, setFormData] = useState([]);
-  let formData;
+  const [formData, setFormData] = useState([]);
 
   useEffect(() => {
     axios.get('/checkout')
-      .then(result => { formData = result.data });
+      .then(result => { setFormData(result.data) });
   }, []);
 
   const handlePurchase = (e) => {
@@ -25,12 +24,48 @@ const ConfirmationForm = ({ formNum, setFormNum }) => {
         </thead>
         <tbody>
           <tr>
-            <td>Name:</td><td>{formData.username /* having trouble here because I want to use the state to show the data but it is trying to render the data from result before it has been returned so it is checking an undefined formData array initially before useEffect runs */}</td>
+            <td>Name:</td><td>{!formData.length ? '' : formData[0].username}</td>
+          </tr>
+          <tr>
+            <td>Email:</td><td>{!formData.length ? '' : formData[0].email}</td>
+          </tr>
+          <tr>
+            <td>Password:</td><td>{!formData.length ? '' : formData[0].password}</td>
+          </tr>
+          <tr>
+            <td>Address Line 1:</td><td>{!formData.length ? '' : formData[0].addressline1}</td>
+          </tr>
+          <tr>
+            <td>Address Line 2:</td><td>{!formData.length ? '' : formData[0].addressline2}</td>
+          </tr>
+          <tr>
+            <td>City:</td><td>{!formData.length ? '' : formData[0].city}</td>
+          </tr>
+          <tr>
+            <td>State:</td><td>{!formData.length ? '' : formData[0].state}</td>
+          </tr>
+          <tr>
+            <td>Zip Code:</td><td>{!formData.length ? '' : formData[0].zip}</td>
+          </tr>
+          <tr>
+            <td>Phone Number:</td><td>{!formData.length ? '' : formData[0].phone}</td>
+          </tr>
+          <tr>
+            <td>Credit Card Number:</td><td>{!formData.length ? '' : formData[0].ccnumber}</td>
+          </tr>
+          <tr>
+            <td>Expiration Date:</td><td>{!formData.length ? '' : formData[0].expiration}</td>
+          </tr>
+          <tr>
+            <td>CVV Code:</td><td>{!formData.length ? '' : formData[0].cvv}</td>
+          </tr>
+          <tr>
+            <td>Billing Zip Code:</td><td>{!formData.length ? '' : formData[0].billingzip}</td>
           </tr>
         </tbody>
       </table>
       <button onClick={handlePurchase}>Purchase</button>
-      <button>Reset</button>
+      {/* <button>Reset</button> */}
     </div>
   );
 }
